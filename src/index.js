adc384c1bbd206d8adcd72794d670eb90029c562
ne0rad <usackasr@gmail.com>
2021-10-06 12:03:54 +0100

    Removed unused imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import { pageLoad } from "./modules/pageLoad";
-import { homeLoad } from "./modules/homeLoad";
-import { menuLoad } from "./modules/menuLoad";
-import { contactsLoad } from "./modules/contactsLoad";
 import { changeTab } from "./modules/changeTab";
 
 
