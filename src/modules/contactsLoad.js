7247181d373686d41c887e489be28d886620bd49
ne0rad <usackasr@gmail.com>
2021-10-07 10:16:51 +0100

    Added Contacts tab

--- src/modules/contactsLoad.js
+++ src/modules/contactsLoad.js
@@ -1,10 +1,31 @@
+import tomatoPhone from '../images/tomato-phone.png';
+
 function contactsLoad() {
+
+    function addContact(title, value) {
+        // function to easily add menu items
+        let contactsText = document.createElement('div');
+        contactsText.classList.add('text-menu');
+        contactsText.textContent = `${title}:   ${value}`;
+        contactsPage.appendChild(contactsText);    
+    }
+
+
     let pageContent = document.getElementById('pageContent');
 
+    // Main menu div
     let contactsPage = document.createElement('div');
-    contactsPage.textContent = "THIS IS CONTACTS PAGE";
-
+    contactsPage.classList.add('contacts-page');
     pageContent.appendChild(contactsPage);
+
+    // Add image
+    let phoneImage = new Image();
+    phoneImage.src = tomatoPhone;
+    phoneImage.classList.add('page-image');
+    contactsPage.appendChild(phoneImage);
+
+    addContact('Email','tomatobar@fruits.com');
+    addContact('Phone','+44 4444 1234');
 }
 
 export { contactsLoad };
